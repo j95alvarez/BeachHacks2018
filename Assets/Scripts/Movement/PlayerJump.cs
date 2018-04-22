f2d58afa13d0a644cee67cc346fd485043c5a374
nkawong <nkawong224@gmail.com>
2018-04-22 04:41:22 -0700

    PlayerJump Completed

---
+++ Assets/Scripts/Movement/PlayerJump.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : AbstractJump {
+    [SerializeField]
+    private int JumpForce;
+    [SerializeField]
+    private int gravity;
+
+    private bool isGrounded = true;
+    Rigidbody rb;
+    
+
+    private void FixedUpdate() {
+        Jump();
+    }
+
+    protected override void Jump() {
+        var zMovement = Input.GetKeyDown("space");
+        rb = GetComponent<Rigidbody>();
+
+        if(isGrounded && zMovement) {
+            rb.AddForce(Vector3.up*JumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Ground")) {
+            isGrounded = true;
+        }
+    }
+}
+
