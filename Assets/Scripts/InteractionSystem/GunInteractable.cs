a6fc86f9546e88468a9e2b8fffab13cb4a562f0f
James Davis <Jamesmdavis95@gmail.com>
2018-04-21 19:05:58 -0700

    Interaction System

--- Assets/Scripts/InteractionSystem/GunInteractable.cs
+++ Assets/Scripts/InteractionSystem/GunInteractable.cs
@@ -4,5 +4,11 @@ using UnityEngine;
 
 namespace TheSquad.InteractionSystem
 {
-    public class 
+    public class GunInteractable : MonoBehaviour, IInteractable
+    {
+        public void Interact()
+        {
+            //Player Picks Up Gun
+        }
+    }
 }
