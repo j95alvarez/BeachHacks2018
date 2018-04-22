da41a17028607cd90331c058a15bf1d767438cd7
j95alvarez <juanalvarez391@yahoo.com>
2018-04-21 17:09:13 -0700

    Made player be able to shoot

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -25,10 +25,16 @@ public class PlayerController : NetworkBehaviour {
         transform.Rotate(0, horizontal, 0);
         transform.Translate(0, 0, vertical);
 
-        if (Input.GetKeyDown(KeyCode.Space)) { Fire(); }
+        if (Input.GetKeyDown(KeyCode.Space)) { CmdFire(); }
     }
 
-    void Fire() {
+    // The [Command] attribute indicates that the following function will be
+    // called by the Client, but will be run on the Server. Any arguments in 
+    // the function will automatically be passed to the Server with the 
+    // Command. Commands can only be sent from the local player object. 
+    // When making a networked command, the function name must begin with “Cmd”.
+    [Command]
+    void CmdFire() {
         // Create the Bullet from the Bullet Prefab
         var bullet = (GameObject)Instantiate(
             bulletPrefab,
@@ -37,6 +43,17 @@ public class PlayerController : NetworkBehaviour {
 
         // Add velocity to the bullet
         bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
+        // The next concept, one that is somewhat unique to Multiplayer Networking,
+        // is the concept of Network Spawning. In the Multiplayer Networking HLAPI
+        // “Spawn” means more than just “Instantiate”. It means to create a GameObject
+        // on the Server and on all of the Clients connected to the Server. The GameObject
+        // will then be managed by the spawning system; state updates are sent to Clients 
+        // when the object changes on the Server, the GameObject will be destroyed on Clients
+        // when it is destroyed on the Server, and the spawned GameObject be added to the set
+        // of networked GameObjects that the Server is managing so that if another Client joins
+        // the game later, the objects will also be spawned on that Client in the correct state.
+        NetworkServer.Spawn(bullet);
 
         // Destroy the bullet after 2 seconds
         Destroy(bullet, 2.0f);
